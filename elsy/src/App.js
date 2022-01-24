3f29e8041a22792385cb4e1443ecccb712317d3a
Nicko <nickos@iMac.local>
2022-01-24 15:57:01 +0100

    put steps Icon & styles

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Steps from "./Components/Steps";
+
+const tempMin = -20,
+tempMax = 40,
+heartMin = 80,
+heartMax = 180,
+stepsMin = 0,
+stepsMax = 50000;
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1 className="text-primary">Hello !</h1>
+      <div className="container-fluid">
+
+        <div className= "row">
+          <p>Heart : {heartMin}</p>
+          <p>Temperature : {tempMin}</p>
+          <Steps />
+          <p>Steps : {stepsMin}</p>
+          
+        </div>
+
       </div>
     );
   }
---
+++ elsy/src/Components/Steps.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Styles from "./steps.modules.css";
+
+class Steps extends React.Component {
+
+  render() {
+
+    return (
+
+      <div className="box col-sm-3 col-6">
+        <span className={"material-icons"}>directions_walk</span>
+      </div>
+
+    );
+
+  }
+
+}
+
+export default Steps;
