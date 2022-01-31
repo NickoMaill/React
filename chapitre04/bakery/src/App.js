daecabcdd1839f94717df1fbf9ba971e61cc55ed
Nicko <nickos@iMac.local>
2022-01-31 14:46:02 +0100

    bakery first commit

---
+++ chapitre04/bakery/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  
+  render() {
+
+    return(
+      <div></div>
+    )
+  
+  }
+
+};
+
+export default App;
