97e1478fcd62f472fbbdda517d07a13d3ce85e75
Nicko <nickos@iMac.local>
2022-01-31 11:07:05 +0100

    add some bootstrap style

--- chapitre04/form/src/Components/form.js
+++ chapitre04/form/src/Components/form.js
@@ -7,35 +7,54 @@ class Form extends React.Component {
     render() {
         return (
 
-            <form>
+            <div>
 
-                <h1>Login</h1>
+            <div className="d-flex flex-column align-items-center">
 
-                <div class="mb-3">
+                <div className="p-5">
 
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-
-                </div>
-                
-                <div class="mb-3">
-
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <h1>Login</h1>
 
                 </div>
 
-                <div class="mb-3 form-check">
+                <form className="w-25">
 
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+
+                    <div class="mb-3">
+
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email..." />
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
+                    </div>
+
+                    <div class="mb-3">
+
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password..."/>
+
+                    </div>
+
+                    <div class="mb-3 form-check">
+
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+
+                <div className="d-none">
+
+                    <h1>Form Submitted</h1>
 
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            </div>
 
-            </form>
         )
     }
 
