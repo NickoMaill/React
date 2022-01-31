59c9bb6ac72edc13236997928653cba742539d63
Nicko <nickos@iMac.local>
2022-01-31 11:32:29 +0100

    creat state

--- chapitre04/form/src/Components/form.js
+++ chapitre04/form/src/Components/form.js
@@ -7,52 +7,53 @@ class Form extends React.Component {
     render() {
         return (
 
-            <div>
+            <div className="">
 
-            <div className="d-flex flex-column align-items-center">
+                <div className="d-flex flex-column align-items-center">
 
-                <div className="p-5">
+                    <div className="p-5">
 
-                    <h1>Login</h1>
+                        <h1>Login</h1>
+
+                    </div>
+
+                    <form className={`w-25 ${this.props.display2}`}>
+
+
+                        <div class="mb-3">
+
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" className={`form-control ${this.props.mailValid}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email..." />
+                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+
+                        </div>
+
+                        <div class="mb-3">
+
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" className={`form-control ${this.props.passValid}`} id="exampleInputPassword1" placeholder="Enter password..." />
+
+                        </div>
+
+                        <div class="mb-3 form-check">
+
+                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                            <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+
+                        </div>
+
+                        <button type="submit" className="btn btn-primary" onChange={this.props.input}>Submit</button>
+
+                    </form>
+
+                    <div className={`bg-secondary p-5 ${this.props.display}`}>
+
+                        <h1 className="p-5">Form Submitted</h1>
+
+                    </div>
 
                 </div>
 
-                <form className="w-25">
-
-
-                    <div class="mb-3">
-
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email..." />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-
-                    </div>
-
-                    <div class="mb-3">
-
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password..."/>
-
-                    </div>
-
-                    <div class="mb-3 form-check">
-
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Submit</button>
-
-                </form>
-
-                <div className="d-none">
-
-                    <h1>Form Submitted</h1>
-
-                </div>
-
-            </div>
             </div>
 
         )
