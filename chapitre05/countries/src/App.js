1c9f35ddb37c7caa345c130e6bbed9abecf3db82
Nicko <nickos@iMac.local>
2022-02-02 14:00:33 +0100

    getCountries method (not worjing)

--- chapitre05/countries/src/App.js
+++ chapitre05/countries/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Button from './Components/Button';
 
 
 class App extends React.Component {
@@ -17,28 +18,61 @@ class App extends React.Component {
 
     }
 
+    this.getCountry = this.getCountry.bind(this)
+
   }
 
   componentDidMount() {
-		fetch("https://restcountries.com/v3.1/name/france")
-			.then(res => res.json())
-			.then(data => {
 
-        data.map((country) =>{
+    fetch("https://restcountries.com/v3.1/name/france")
+      .then(res => res.json())
+      .then(data => {
 
-          console.log("data", country);
-          this.setState({ 
-            name: country.name.common, 
+        data.map((country) => {
+
+          // console.log("data", country);
+
+          this.setState({
+            name: country.name.common,
             capital: country.capital,
             flag: country.flags.png,
             population: country.population,
             region: country.region
 
           });
+
         })
 
-			});
-	}
+      });
+
+
+  }
+
+  // componentDidUpdate(){
+  //   this.getCountry();
+  // }
+  
+  async getCountry(country) {
+    
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(res => res.json())
+    .then(data => {
+      
+      data.map((countrySearch) => {
+        
+        this.setState({
+          name: countrySearch.name.common,
+          capital: countrySearch.capital,
+          flag: countrySearch.flags.png,
+          population: countrySearch.population,
+          region: countrySearch.region
+          
+        });
+        
+      });
+    })
+  }
+  
 
   render() {
 
@@ -46,14 +80,40 @@ class App extends React.Component {
 
       <div>
 
-        <ul>
+        <div>
 
-          <li><img src={`${this.state.flag}`}/></li>
-          <li>Name : {this.state.name}</li>
-          <li>Capital : {this.state.capital}</li>
-          <li>Population : {this.state.population}</li>
-          <li>Region : {this.state.region}</li>
-        </ul>
+          <Button
+            children="France"
+            // onClick={this.getCountry("france")}
+          />
+
+          <Button
+            children="Brazil"
+            // onClick={this.getCountry("brazil")}
+          />
+
+          <Button
+            children="Croatia"
+            // onClick={this.getCountry("croatia")}
+          />
+
+        </div>
+
+        <div>
+
+          <img src={`${this.state.flag}`} />
+
+          <ul>
+
+            <li>Name : {this.state.name}</li>
+            <li>Capital : {this.state.capital}</li>
+            <li>Population : {this.state.population}</li>
+            <li>Region : {this.state.region}</li>
+
+          </ul>
+
+        </div>
+
 
       </div>
 
@@ -63,4 +123,4 @@ class App extends React.Component {
 
 }
 
-export default App
+export default App;
---
+++ chapitre05/countries/src/Components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Button extends React.Component {
+
+    render() {
+
+        return(
+
+            <div>
+
+                <button onClick={this.props.onClick}>{this.props.children}</button>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default Button;
