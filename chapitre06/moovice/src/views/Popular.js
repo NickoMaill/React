05562de302589e07f665f90bd110e8c9faf0b005
Nicko <nicomaillols@gmail.com>
2022-02-15 17:10:27 +0100

    creat all views

---
+++ chapitre06/moovice/src/views/Popular.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Popular extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Popular;
---
+++ chapitre06/moovice/src/views/PopularBattle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PopularBattle extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PopularBattle;
