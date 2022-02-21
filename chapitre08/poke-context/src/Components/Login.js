2722fb71b9091cbdd8966a3bfdf08d0c75de5ee1
Nicko <nickos@iMac.local>
2022-02-21 14:33:02 +0100

    set form in login.js

--- chapitre08/poke-context/src/Components/Login.js
+++ chapitre08/poke-context/src/Components/Login.js
@@ -1,5 +1,27 @@
-export default function Login () {
-    return(
-        <h1>LOGIN</h1>
+import { useForm } from "react-hook-form";
+
+
+export default function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data);
+
+    return (
+        <div>
+
+            <h1>LOGIN</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <input {...register("userName", { required: true }, {maxLength: 15})}/>
+                {errors.userName && <span>you must enter your username</span>}
+                <input {...register("password", { required: true }, {minLength: 6})}/>
+                {errors.password && <span>you must enter your password</span>}
+
+            </form>
+        </div>
     )
 }
